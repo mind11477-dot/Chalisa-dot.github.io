c9b1c667996d10c97ea65305076a8b94e28aba37
mind11477-dot <mind11477@gmail.com>
2026-06-16 10:30:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
+:::writing{variant="standard" id="53172"}
 const dog = document.getElementById("dog");
 const bone = document.getElementById("bone");
 
 bone.addEventListener("click", () => {
 
-```
-const x = Math.random() * (window.innerWidth - 150);
-const y = 200 + Math.random() * 300;
+    const x = Math.random() * (window.innerWidth - 150);
+    const y = 200 + Math.random() * 300;
 
-bone.style.left = x + "px";
-bone.style.top = y + "px";
+    bone.style.left = x + "px";
+    bone.style.top = y + "px";
 
-setTimeout(() => {
-    dog.style.left = x + "px";
-    dog.style.top = y + "px";
-},300);
-```
+    setTimeout(() => {
+        dog.style.left = x + "px";
+        dog.style.top = y + "px";
+    }, 300);
 
 });
+:::
+
+แล้วใน `index.html` ต้องมี
+
+```html
+<div id="dog">🐶</div>
+<div id="bone">🦴</div>
+
+<script src="script.js"></script>
