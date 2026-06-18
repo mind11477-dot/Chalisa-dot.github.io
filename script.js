745f28c4f6ba1236c3ccbd6dd872d0c98948b404
mind11477-dot <mind11477@gmail.com>
2026-06-18 14:12:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const container = document.querySelector(".container");
 
-function goNext(){
-  container.style.transform = "translateX(-100vw)";
-}
-
-function goBack(){
-  container.style.transform = "translateX(0)";
-}
