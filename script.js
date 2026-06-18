56dcf456b8d305c52e0f18cedd76354db963084a
mind11477-dot <mind11477@gmail.com>
2026-06-18 14:01:39 +0700

    Add navigation functions to move container

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const container = document.querySelector(".container");
 
+function goNext(){
+  container.style.transform = "translateX(-100vw)";
+}
+
+function goBack(){
+  container.style.transform = "translateX(0)";
+}
